f8d10918baece6d38601b6bb86b95282f43b920d
jerryjerry2 <chmapikeat@gmail.com>
2026-05-20 00:09:26 +0700

    fix bugs

--- config/db.js
+++ config/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
     host : process.env.DB_HOST,
-    user : process.env.USER,
-    password : process.env.PASSWORD,
-    database : process.env.DATABASE
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_DATABASE
 });
 
 module.exports = pool;
