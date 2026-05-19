33033c63e43d7b872652c2c160d60b991ef1a15b
jerryjerry2 <chmapikeat@gmail.com>
2026-05-20 00:18:51 +0700

    add code

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const auth = require('./routes/auth');
 
 const app = express();
 
 app.use(express.json());
-require('dotenv').config();
+
 
 app.use('/api/auth', auth);
 
